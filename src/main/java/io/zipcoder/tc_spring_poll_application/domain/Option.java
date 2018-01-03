c0d84222d9dfbc620dcb82fe1e59b3b77081a5ed
Brian R Mullin <brmullin@email.msmary.edu>
2018-01-03 11:42:30 -0500

    Added Poll class with getters and setters

--- src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
@@ -1,6 +1,7 @@
 package io.zipcoder.tc_spring_poll_application.domain;
 
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -13,5 +14,24 @@ public class Option {
     @Column(name = "OPTION_ID")
     private Long id;
 
+    @Column(name = "OPTION_VALUE")
+    private String value;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
 
 }
---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Poll.java
@@ -0,0 +1,49 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+
+import org.springframework.core.annotation.Order;
+
+import javax.persistence.*;
+
+
+@Entity
+public class Poll {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "POLL_ID")
+    private Long id;
+
+    @Column(name = "QUESTION")
+    private String question;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "POLL_ID")
+    @OrderBy
+    private Option options;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(String question) {
+        this.question = question;
+    }
+
+    public Option getOptions() {
+        return options;
+    }
+
+    public void setOptions(Option options) {
+        this.options = options;
+    }
+    
+}
